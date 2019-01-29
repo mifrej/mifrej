5732fe9a4958adcb512ab9110600c2af4339cfbd
Milosz Frejnik <milosz@frejnik.com>
2019-01-29 21:34:38 +0100

    Improve styling

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,42 +5,42 @@ import './contact.css';
 
 
 export default withSiteData(() => (
-  <section>
-    <h3>Let's keep in touch</h3>
+  <section className="contact-section">
+    <h2>Let's keep in touch</h2>
     <address>
       <ul>
         <li>
-          <a href="https://www.linkedin.com/in/mifrej/">
+          <a href="https://www.linkedin.com/in/mifrej/" title="linkedin">
             <Icon name="svg-icon-linkedin" />
             <em className="visuallyhidden">linkedin</em>
           </a>
         </li>
         <li>
-          <a href="https://github.com/mifrej">
+          <a href="https://github.com/mifrej" title="github">
             <Icon name="svg-icon-github" />
             <em className="visuallyhidden">github</em>
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/mifrej">
+          <a href="https://twitter.com/mifrej" title="twitter">
             <Icon name="svg-icon-twitter" />
             <em className="visuallyhidden">twitter</em>
           </a>
         </li>
         <li>
-          <a href="https://www.facebook.com/mifrej">
+          <a href="https://www.facebook.com/mifrej" title="facebook">
             <Icon name="svg-icon-facebook" />
             <em className="visuallyhidden">facebook</em>
           </a>
         </li>
         <li>
-          <a href="mailto:milosz@frejnik.com">
+          <a href="mailto:milosz@frejnik.com" title="email">
             <Icon name="svg-icon-envelop" />
             <em className="visuallyhidden">email</em>
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="#" title="resume">
             <Icon name="svg-icon-profile" />
             <em className="visuallyhidden">Resume</em>
           </a>
