1e1046b2a2571b4f078756d7a9a383419fdd7e7c
Milosz Frejnik <milosz@frejnik.com>
2019-02-06 21:17:03 +0100

    remove autotrack

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import reactDom from 'react-dom';
-import 'autotrack/lib/plugins/event-tracker';
-import 'autotrack/lib/plugins/outbound-link-tracker';
-import 'autotrack/lib/plugins/url-change-tracker';
+import ReactDom from 'react-dom';
 
 // Your top level component
 import App from './App';
@@ -10,19 +7,11 @@ import App from './App';
 // Export your top level component as JSX (for static rendering)
 export default App;
 
-interface ICustomWindow extends Window {
-  ga?: any
-}
-
-const customWindow:ICustomWindow = window;
-
-customWindow.ga = customWindow.ga;
-
 // Render your app
 if (typeof document !== 'undefined') {
   const renderMethod = module.hot
-    ? reactDom.render
-    : reactDom.hydrate || reactDom.render;
+    ? ReactDom.render
+    : ReactDom.hydrate || ReactDom.render;
 
   const render = (Comp: any) => {
     renderMethod(<Comp />, document.getElementById('root'));
@@ -35,16 +24,5 @@ if (typeof document !== 'undefined') {
   if (module.hot) {
     module.hot.accept('./App', () => render(require('./App').default));
   }
-
-  const ga = customWindow.ga;
-  ga('create', 'UA-2176096-15', 'auto');
-
-
-  // Only require the plugins you've imported above.
-  ga('require', 'eventTracker');
-  ga('require', 'outboundLinkTracker');
-  ga('require', 'urlChangeTracker');
-
-  ga('send', 'pageview');
 
 }
